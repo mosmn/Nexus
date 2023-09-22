9e9928c6740fe1669a38a20d5d699a3e8edb5cb7
mosmn <ithinkisee8@gmail.com>
2023-09-22 14:56:39 +0800

    created subject model

---
+++ models/subject.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const SubjectSchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+  code: { type: String, required: true, maxlength: 100 },
+  description: { type: String, maxlength: 100 },
+});
+
+SubjectSchema.virtual("url").get(function () {
+  return "/subjects/" + this._id;
+});
+
+module.exports = mongoose.model("Subject", SubjectSchema);
